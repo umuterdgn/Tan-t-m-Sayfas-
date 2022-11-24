2366d2a2ab8af3af779c7c5317148cb5feb6f3c7
Umut Erdoğan <109750600+umuterdgn@users.noreply.github.com>
2022-11-24 14:35:37 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(
@@ -6,18 +7,89 @@ void main() {
   );
 }
 
-class BenimUyg extends StatefulWidget {
+class BenimUyg extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.amber,
-      body: SafeArea(
-          child: Column(
-        children: <Widget>[
-          CircleAvatar(radius: 70.0),
-        ],
-      )),
-    ));
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 70.0,
+                  backgroundColor: Colors.amber,
+                  backgroundImage: AssetImage('assets/image/umut.jpg'),
+                ),
+                Text(
+                  'yazılımografi',
+                  style: GoogleFonts.pacifico(
+                    fontSize: 45,
+                    color: Colors.brown[900],
+                  ),
+                ),
+                Text(
+                  'BİR TIK UZAĞINIZDA',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  child: Divider(
+                    height: 30,
+                    color: Colors.pinkAccent[900],
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 45.0,
+                  ),
+                  color: Colors.deepPurple[900],
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'erdgnumut21@gmail.com',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 45.0,
+                  ),
+                  color: Colors.deepPurple[900],
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      '+90 505 147 77 29',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
